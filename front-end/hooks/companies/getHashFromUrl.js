a65d1fe86d784eba3a31ee569d439db7cf721b91
Kirill Ruchin <117009127+NeKyReal@users.noreply.github.com>
2023-07-24 23:01:40 +0700

    Delete front-end/hooks directory

--- front-end/hooks/companies/getHashFromUrl.js
+++
@@ -1,7 +0,0 @@
-export function getHashFromUrl (url) {
-  const parts = url.split('/register/')
-
-  const partAfterRegister = parts[1]
-  console.log(partAfterRegister)
-  return partAfterRegister
-}
--- front-end/hooks/currentTime.js
+++
@@ -1,4 +0,0 @@
-const date = new Date()
-const hours = date.getHours().toString().padStart(2, '0')
-const minutes = date.getMinutes().toString().padStart(2, '0')
-export const currentTime = `${hours}:${minutes}`
